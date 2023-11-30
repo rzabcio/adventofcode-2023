42aa8728170c4419a270952ad1d2f5f9c979146b
Jakub Głazik <rzabcio@gmail.com>
2023-11-30 01:12:50 +0100

    feat(day1): solution 1

--- day01.go
+++ day01.go
@@ -1,7 +1,23 @@
 package main
 
+import (
+	"unicode"
+)
+
 func Day01_1(filename string) (result int) {
-	result = 142
+	for line := range inputCh(filename) {
+		var num int
+		for _, r := range line {
+			if unicode.IsDigit(r) {
+				if num == 0 {
+					result += int(r-'0') * 10
+				}
+				num = int(r - '0')
+			}
+		}
+		result += num
+	}
+	// result = 142
 	return result
 }
 
