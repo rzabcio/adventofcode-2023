ac5d021d832188db986c671b133dd70272119da9
Jakub Głazik <rzabcio@gmail.com>
2023-12-16 19:43:42 +0100

    fix(day13): changes for part 2
    code becomes a little too complicated, but needed to find which rows/cols have EXACTLY one differ point
    for every axis row/col combination

--- day13/day13.go
+++ day13/day13.go
@@ -2,6 +2,7 @@ package day13
 
 import (
 	"fmt"
+	"math"
 	"strings"
 
 	"github.com/rzabcio/adventofcode-2023/utils"
@@ -12,12 +13,20 @@ func Day13_1(filename string) (result int) {
 	for _, rp := range reflectionPatterns {
 		rp.findVertAxis()
 		rp.findHorizAxis()
+		fmt.Println(rp)
 		result += rp.vertAxis + rp.horizAxis*100
 	}
 	return result
 }
 
 func Day13_2(filename string) (result int) {
+	reflectionPatterns := readReflectionPatterns(filename)
+	for _, rp := range reflectionPatterns {
+		rp.findVertAxis()
+		rp.findHorizAxis()
+		fmt.Println(rp)
+		result += rp.vertAxis + rp.horizAxis*100
+	}
 	return result
 }
 
@@ -26,7 +35,7 @@ func readReflectionPatterns(filename string) (result []ReflectionPattern) {
 
 	row := 0
 	for line := range utils.InputCh(filename) {
-		if strings.HasPrefix(line, "--- ") {
+		if strings.HasPrefix(line, "- ") {
 			continue
 		}
 		// if empty line - add and new pattern
@@ -63,10 +72,12 @@ type ReflectionPattern struct {
 	rowCount  []int
 	vertAxis  int
 	horizAxis int
+	rowDiffs  [][]int
+	colDiffs  [][]int
 }
 
 func (rp ReflectionPattern) String() string {
-	result := fmt.Sprintf("vertAxis: %d, horizAxis: %d\n", rp.vertAxis, rp.horizAxis)
+	result := fmt.Sprintf("--- vertAxis: %d, horizAxis: %d\n", rp.vertAxis, rp.horizAxis)
 	result += "    "
 	for i, col := range rp.colCount {
 		if i > 1 && i == rp.vertAxis-1 {
@@ -95,27 +106,28 @@ func (rp ReflectionPattern) String() string {
 		}
 		result += "\n"
 	}
+
+	if rp.rowDiffs != nil {
+		result += "\n"
+		for row, diffs := range rp.rowDiffs {
+			result += fmt.Sprintf("row %d: %v\n", row, diffs)
+		}
+	}
+	if rp.colDiffs != nil {
+		for col, diffs := range rp.colDiffs {
+			result += fmt.Sprintf("col %d: %v\n", col, diffs)
+		}
+	}
+
 	return result
 }
 
 func (rp *ReflectionPattern) findVertAxis() {
 	possibleVertAxis := []int{}
-NEXT_AXIS:
 	for col := 0; col < len(rp.colCount)-1; col++ {
-		for i := 0; ; i++ {
-			if col-i < 0 || col+i+1 >= len(rp.colCount) {
-				break
-			}
-			if rp.colCount[col-i] != rp.colCount[col+i+1] {
-				continue NEXT_AXIS
-			}
-			for row := 0; row < len(rp.rowCount); row++ {
-				if rp.pattern[row][col-i] != rp.pattern[row][col+i+1] {
-					continue NEXT_AXIS
-				}
-			}
+		if rp.isColAxis(col) {
+			possibleVertAxis = append(possibleVertAxis, col)
 		}
-		possibleVertAxis = append(possibleVertAxis, col)
 	}
 	if len(possibleVertAxis) > 0 {
 		rp.vertAxis = possibleVertAxis[0] + 1
@@ -128,22 +140,10 @@ NEXT_AXIS:
 
 func (rp *ReflectionPattern) findHorizAxis() {
 	possibleHorizAxis := []int{}
-NEXT_AXIS:
 	for row := 0; row < len(rp.rowCount)-1; row++ {
-		for i := 0; ; i++ {
-			if row-i < 0 || row+i+1 >= len(rp.rowCount) {
-				break
-			}
-			if rp.rowCount[row-i] != rp.rowCount[row+i+1] {
-				continue NEXT_AXIS
-			}
-			for col := 0; col < len(rp.colCount); col++ {
-				if rp.pattern[row-i][col] != rp.pattern[row+i+1][col] {
-					continue NEXT_AXIS
-				}
-			}
+		if rp.isRowAxis(row) {
+			possibleHorizAxis = append(possibleHorizAxis, row)
 		}
-		possibleHorizAxis = append(possibleHorizAxis, row)
 	}
 	if len(possibleHorizAxis) > 0 {
 		rp.horizAxis = possibleHorizAxis[0] + 1
@@ -152,4 +152,66 @@ NEXT_AXIS:
 		fmt.Printf("!!! error - there is more than one possible horizontal axis: %d\n", possibleHorizAxis)
 		fmt.Println(rp)
 	}
+}
+
+func (rp *ReflectionPattern) isColAxis(col int) bool {
+	return rp.isColAxisDiffNotGreaterThan(col, 0) == 0
+}
+
+func (rp *ReflectionPattern) isColAxisDiffNotGreaterThan(col, maxDiff int) (colDiff int) {
+	for i := 0; ; i++ {
+		if col-i < 0 || col+i+1 >= len(rp.colCount) {
+			break
+		}
+		colDiff += rp.findColsDiff(col-i, col+i+1)
+		if colDiff > maxDiff {
+			return colDiff
+		}
+	}
+	return colDiff
+}
+
+func (rp *ReflectionPattern) findColsDiffs(col1, col2 int) (result []int) {
+	for row := 0; row < len(rp.rowCount); row++ {
+		result = append(result, rp.pattern[row][col1]-rp.pattern[row][col2])
+	}
+	return result
+}
+
+func (rp *ReflectionPattern) findColsDiff(col1, col2 int) (result int) {
+	for _, row := range rp.findColsDiffs(col1, col2) {
+		result += int(math.Abs(float64(row)))
+	}
+	return result
+}
+
+func (rp *ReflectionPattern) isRowAxis(row int) bool {
+	return rp.isRowAxisDiffNotGreaterThan(row, 0) == 0
+}
+
+func (rp *ReflectionPattern) isRowAxisDiffNotGreaterThan(row, maxDiff int) (rowDiff int) {
+	for i := 0; ; i++ {
+		if row-i < 0 || row+i+1 >= len(rp.rowCount) {
+			break
+		}
+		rowDiff += rp.findRowsDiff(row-i, row+i+1)
+		if rowDiff > maxDiff {
+			return rowDiff
+		}
+	}
+	return rowDiff
+}
+
+func (rp *ReflectionPattern) findRowsDiffs(row1, row2 int) (result []int) {
+	for col := 0; col < len(rp.colCount); col++ {
+		result = append(result, rp.pattern[row1][col]-rp.pattern[row2][col])
+	}
+	return result
+}
+
+func (rp *ReflectionPattern) findRowsDiff(row1, row2 int) (result int) {
+	for _, col := range rp.findRowsDiffs(row1, row2) {
+		result += int(math.Abs(float64(col)))
+	}
+	return result
 }
