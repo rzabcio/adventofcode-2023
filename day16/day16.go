bc64b385935acc06a0034c9426e02555d58df9e1
Jakub Głazik <rzabcio@gmail.com>
2023-12-22 17:44:09 +0100

    feat(day16) solution part 2
    both parts are brute force, but at least they are quick :)

--- day16/day16.go
+++ day16/day16.go
@@ -13,6 +13,27 @@ func Day16_1(filename string) (result int) {
 }
 
 func Day16_2(filename string) (result int) {
+	c := NewContraption(filename)
+	for startX := 0; startX < len(c.grid[0]); startX++ {
+		subResult := c.Run(startX, -1)
+		if subResult > result {
+			result = subResult
+		}
+		subResult = c.Run(startX, len(c.grid))
+		if subResult > result {
+			result = subResult
+		}
+	}
+	for startY := 0; startY < len(c.grid); startY++ {
+		subResult := c.Run(-1, startY)
+		if subResult > result {
+			result = subResult
+		}
+		subResult = c.Run(len(c.grid[0]), startY)
+		if subResult > result {
+			result = subResult
+		}
+	}
 	return result
 }
 
@@ -48,7 +69,18 @@ func (c *Contraption) Run(startX, startY int) (result int) {
 
 func (c *Contraption) Reset() {
 	c.beams = []*Beam{}
-	c.beams = append(c.beams, &Beam{x: c.startX, y: c.startY, dX: 1, dY: 0})
+	var dX, dY int
+	switch {
+	case c.startX == -1:
+		dX, dY = 1, 0
+	case c.startX == len(c.grid[0]):
+		dX, dY = -1, 0
+	case c.startY == -1:
+		dX, dY = 0, 1
+	case c.startY == len(c.grid):
+		dX, dY = 0, -1
+	}
+	c.beams = append(c.beams, &Beam{x: c.startX, y: c.startY, dX: dX, dY: dY})
 	for y := range c.visitVert {
 		for x := range c.visitVert[y] {
 			c.visitVert[y][x] = false
